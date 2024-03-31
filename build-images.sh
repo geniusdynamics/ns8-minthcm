eded9656352555cb39f30d6f8edb872e3fa41fbf
kemboi22 <kemboielvis22@gmail.com>
2024-03-31 23:42:59 +0300

    Added neccessary images needed

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/nginx:stable-alpine3.17" \
+    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/minthcm/minthcm docker.io/elasticsearch" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
