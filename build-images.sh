964276310885c1b4999c157a127a410ae250c3d6
kemboi22 <kemboielvis22@gmail.com>
2024-03-31 23:49:37 +0300

    Changes in repo name

--- build-images.sh
+++ build-images.sh
@@ -11,7 +11,7 @@ set -e
 # Prepare variables for later use
 images=()
 # The image will be pushed to GitHub container registry
-repobase="${REPOBASE:-ghcr.io/kemboi22}"
+repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="minthcm"
 
